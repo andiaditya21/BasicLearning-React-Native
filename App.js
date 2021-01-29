52a8cecb94290a92bb225fb270e170090eb32e32
andiaditya21 <andiaditya21@gmail.com>
2021-01-29 15:51:18 +0800

    basic 6 component RN

--- App.js
+++ App.js
@@ -1,77 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Image, Text, View, TextInput} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
-      <View>
-        <Text>Nyamanna</Text>
-        <Text>Kadang hot reload kadang ga yaa-_-</Text>
-      </View>
-    </>
+    <View>
+      <View style={{backgroundColor: 'blue', width: 100, height: 100}} />
+      <Text>Andi</Text>
+      <Text>Aditya</Text>
+      <Text>Mega</Text>
+      <Anita />
+      <Photo />
+      <TextInput style={{borderWidth: 1}} />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Anita = () => {
+  return <Text>Anita</Text>;
+};
+
+const Photo = () => {
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/200/200/people'}}
+      style={{width: 200, height: 200}}
+    />
+  );
+};
 
 export default App;
